91526dc404956cbb31833e1773f7c4011ddea0a4
Michael Bussmann <bus@mb-net.net>
1998-01-17 13:32:22 +0000

    Added CVS header

--- prefixmatch.c
+++ prefixmatch.c
@@ -1,3 +1,8 @@
+/*
+ * prefixmatch.c -- Some stupid routines for PostgreSQL
+ *
+ * $Id: prefixmatch.c,v 1.2 1998/01/17 13:32:22 bus Exp $
+ */
 #include <string.h>
 #include "postgres.h"
 #include "utils/palloc.h"
