093429d4fc30a0fe1f6807c8f9a2b1e927ac3ee0
Michael Bussmann <bus@mb-net.net>
1998-01-17 13:29:56 +0000

    Imported prototypes from changed files

--- euracom.h
+++ euracom.h
@@ -3,12 +3,12 @@
  *
  * euracom.h -- Various function prototypes and externals
  *
- * Copyright (C) 1996-1997 by Michael Bussmann
+ * Copyright (C) 1996-1998 by Michael Bussmann
  *
  * Authors:             Michael Bussmann <bus@fgan.de>
- * Created:             1997-10-04 17:30:00 GMT
- * Version:             $Revision: 1.8 $
- * Last modified:       $Date: 1997/10/04 16:51:01 $
+ * Created:             1997-10-27 09:30:04 GMT
+ * Version:             $Revision: 1.9 $
+ * Last modified:       $Date: 1998/01/17 13:29:56 $
  * Keywords:            ISDN, Euracom, Ackermann, PostgreSQL
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -21,6 +21,32 @@
  * or fitness for a particular purpose.  See the GNU Public Licence for
  * more details.
  **************************************************************************/
+
+#if !defined(_EURACOM_H)
+#define _EURACOM_H
+
+/* Use local configuration */
+#include "config.h"
+
+/*
+ * select which terminal handling to use (currently only SysV variants)
+ */
+#if defined(HAVE_TERMIOS)
+#include <termios.h>
+#define TTY_GETATTR(_FD_, _ARG_) tcgetattr((_FD_), (_ARG_))
+#define TTY_SETATTR(_FD_, _ARG_) tcsetattr((_FD_), TCSANOW, (_ARG_))
+#endif
+
+#if defined(HAVE_TERMIO)
+#include <termio.h>
+#include <sys/ioctl.h>
+#define TTY_GETATTR(_FD_, _ARG_) ioctl((_FD_), TCGETA, (_ARG_))
+#define TTY_SETATTR(_FD_, _ARG_) ioctl((_FD_), TCSETAW, (_ARG_))
+#endif
+
+#ifndef TTY_GETATTR
+#error MUST DEFINE ONE OF "HAVE_TERMIOS" or "HAVE_TERMIO"
+#endif
 
 /* Database subsystem: postgres.c */
 extern void database_set_host(const char *str);
@@ -36,12 +62,20 @@ extern void database_check_state();
 extern BOOLEAN database_log(const char *cp);
 
 /* Serial subsystem: serial.c */
-extern void serial_set_protocol_name(const char *str);
-extern void serial_set_device(const char *str);
-extern int serial_query_fd();
+struct SerialFile {
+  int  fd;
+  char *protocol_filename;
+  char *fd_device;
+  struct termios term;
+};
 
-extern BOOLEAN serial_initialize();
-extern int serial_shutdown();
+extern struct SerialFile *serial_allocate_file(void);
+extern void serial_deallocate_file(struct SerialFile *sf);
+extern void serial_set_protocol_name(struct SerialFile *, const char *str);
+extern void serial_set_device(struct SerialFile *, const char *str);
+extern int serial_query_fd(const struct SerialFile *);
+extern BOOLEAN serial_open_device(struct SerialFile *sf);
+extern BOOLEAN serial_close_device(struct SerialFile *sf);
+extern char *readln_rs232(struct SerialFile *sf);
 
-extern char *readln_rs232();
-
+#endif
