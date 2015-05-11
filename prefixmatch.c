610a84c07bb770c19f80db5d4ef379e34c8af108
Michael Bussmann <bus@mb-net.net>
2015-05-11 15:59:13 +0200

    PostgreSQL 9.4

--- prefixmatch.c
+++ prefixmatch.c
@@ -5,8 +5,8 @@
  */
 
 #include "postgres.h"
+#include "fmgr.h"
 #include <string.h>
-#include "9.1/server/fmgr.h"
 
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
@@ -17,7 +17,7 @@ bool prefix_match(text *t1, text *t2)
   return (strncasecmp(VARDATA(t1), VARDATA(t2), VARSIZE(t2)-VARHDRSZ)==0);
 }
 
-int2 length(text *t1)
+int length(text *t1)
 {
   return (VARSIZE(t1)-VARHDRSZ);
 }
